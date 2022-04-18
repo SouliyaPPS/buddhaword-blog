cb2d6a894c4d2a0fb438cb9e7afdbd53d3fbbcc2
Souliya PPS <souliyappsdev@gmail.com>
2022-04-18 21:52:06 +0700

    new

--- Backend/Helpers/query/queryHelpers.js
+++ Backend/Helpers/query/queryHelpers.js
@@ -1,40 +1,46 @@
 const searchHelper = (searchKey, query, req) => {
-  if (req.query.search) {
-    const searchObject = {};
 
-    const regex = new RegExp(req.query.search, "i");
+    if (req.query.search) {
 
-    searchObject[searchKey] = regex;
+        const searchObject = {};
 
-    query = query.where(searchObject);
+        const regex = new RegExp(req.query.search, "i")
+
+        searchObject[searchKey] = regex
+
+        query = query.where(searchObject);
+      
+        return query
+    }
 
     return query;
-  }
+}
 
-  return query;
-};
 
-const paginateHelper = async (model, query, req) => {
-  const page = parseInt(req.query.page) || 1;
-  const pageSize = parseInt(req.query.limit) || 6;
-  const skip = (page - 1) * pageSize;
+const paginateHelper = async (model ,query ,req)=> {
 
-  const regex = new RegExp(req.query.search, "i");
+    const page = parseInt( req.query.page ) || 1 ; 
+    const pageSize = parseInt( req.query.limit ) || 6 ; 
+    const skip  = (page-1 ) * pageSize ; 
+   
+    const regex = new RegExp(req.query.search, "i")    
 
-  const total = await model.countDocuments({ content: regex });
+    const total = await model.countDocuments({"content" : regex})
+  
+    const pages = Math.ceil(total / pageSize )  ;
 
-  const pages = Math.ceil(total / pageSize);
+    query = query.skip(skip).limit(pageSize) ; 
 
-  query = query.skip(skip).limit(pageSize);
+    return {
+        query : query  ,
+        page : page ,
+        pages : pages  
+    }
 
-  return {
-    query: query,
-    page: page,
-    pages: pages,
-  };
-};
+}
 
-module.exports = {
-  searchHelper,
-  paginateHelper,
-};
+
+module.exports ={
+    searchHelper,
+    paginateHelper
+}
