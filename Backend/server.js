8586aebb6fc48d04e4ce7fefb470204ea09cb4be
Souliya PPS <souliyappsdev@gmail.com>
2022-04-18 16:18:34 +0700

    first commit

--- Backend/server.js
+++ Backend/server.js
@@ -24,7 +24,7 @@ app.use(customErrorHandler)
 
 const PORT = process.env.PORT || 5000 ;
 
-app.use(express.static(path.join(__dirname , "public") ))
+app.use(express.static(path.join(__dirname , "build") ))
 
 const server = app.listen(PORT,()=>{
 
